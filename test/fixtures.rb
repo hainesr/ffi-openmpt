695a227536c42350560fedaa31d8037cc4a210d4
Robert Haines <rhaines@manchester.ac.uk>
2020-08-22 13:30:36 +0100

    Break out the fixture constants to their own file.
    We can now pull these in to the Rakefile as well without pulling in the
    whole library.

---
+++ test/fixtures.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2018-2020, Robert Haines.
+#
+# Licensed under the BSD License. See LICENCE for details.
+
+# Fixtures directory.
+FIXTURES_DIR = ::File.expand_path('fixtures', __dir__)
+
+# Test mods.
+MOD_LAST_SUN = ::File.join(FIXTURES_DIR, 'lastsun.mod')
+
+# Raw data.
+RAW_LAST_SUN_INT16 = ::File.join(FIXTURES_DIR, 'lastsun.int16.raw')
+RAW_LAST_SUN_FLOAT = ::File.join(FIXTURES_DIR, 'lastsun.float.raw')
+RAW_LAST_SUN_MONO_INT16 = ::File.join(FIXTURES_DIR, 'lastsun.mono.int16.raw')
+RAW_LAST_SUN_MONO_FLOAT = ::File.join(FIXTURES_DIR, 'lastsun.mono.float.raw')
